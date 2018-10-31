917ab43fd4d7dd8e948ecea4f45ffa9a4d219f60
Ilya <hippiman1221@gmail.com>
2018-10-31 13:26:31 +0300

    переименовал папку

---
+++ Сизов/base/sample_postfix.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include "postfix.h"
+
+using namespace std;
+
+int main()
+{
+	string expression;
+	TPostfix postfix("(((1+23)*1-22)+5)*2-7");
+	double res;
+
+	setlocale(LC_ALL, "Russian");
+	//cout << "Введите арифметическое выражение: ";
+	//cin >> expression;
+	//cout << expression << endl;
+	cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
+	postfix.ToPostfix();
+	cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
+	res = postfix.Calculate();
+	cout << res << endl;
+	return 0;
+}
